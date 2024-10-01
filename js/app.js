722f617d09b6387c7ac925f8eb5d551ce81db36a
alisher <alisher@gmail.com>
2024-10-01 12:50:06 +0500

    dars5_masala

---
+++ js/app.js
@@ -0,0 +1,299 @@
+//1-masala;
+
+// let num = +prompt("sonni kiriting ...");
+
+// if (num > 0) {
+//   ++num;
+// }
+// console.log(num);
+
+//2-masala;
+
+// let a = +prompt("sonni kriting ...");
+
+// if (a > 0) {
+//   ++a;
+// } else {
+//   --a;
+// }
+// console.log(a);
+
+//3-masala;
+
+// let a = +prompt("sonni kriting");
+
+// if (a > 0) {
+//   ++a;
+// } else if (a == 0) {
+//   a = 10;
+// } else {
+//   a = a - 2;
+// }
+// console.log(a);
+
+//4-masala;
+
+// let a = +prompt("1son");
+// let b = +prompt("2son");
+// let c = +prompt("3son");
+
+// let count = 0;
+
+// if (a > 0) {
+//   count++;
+// }
+
+// if (b > 0) {
+//   count++;
+// }
+
+// if (c > 0) {
+//   count++;
+// }
+
+// console.log(count);
+
+//5-masala;
+
+// let a = +prompt("1son");
+// let b = +prompt("2son");
+// let c = +prompt("3son");
+
+// let manfiy = 0;
+// let musbat = 0;
+
+// if (a > 0) {
+//   musbat++;
+// } else {
+//   manfiy++;
+// }
+
+// if (b > 0) {
+//   musbat++;
+// } else {
+//   manfiy++;
+// }
+
+// if (c > 0) {
+//   musbat++;
+// } else {
+//   manfiy++;
+// }
+
+// console.log(manfiy, musbat);
+
+// 6-masala;
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max=0;
+
+// if (a > b) {
+//     max = a;
+// }else{
+//     max = b;
+// }
+
+// console.log(max);
+
+// 7-masala;
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max=0;
+// let min=0;
+
+// if (a > b) {
+//     max = a;
+//     min = b;
+// }else{
+//     max = b;
+//     min = a;
+// }
+
+// console.log(min, max);
+
+// 8-masala
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max=0;
+// let min=0;
+
+// if (a > b) {
+//     max = a;
+//     min = b;
+// }else{
+//     max = b;
+//     min = a;
+// }
+
+// console.log(max, min);
+
+// 10-masala
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let res = 0;
+
+// if (a == b) {
+//     res = a + b;
+// }else{
+//     res = 0;
+// }
+
+// console.log(res);
+
+// 11-masala
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max = 0;
+
+// if (a > b ) {
+//     max = a;
+// }else if (a < b) {
+//     max = b;
+// }else{
+//     max = 0;
+// }
+
+// console.log(max);
+
+// 12-masala
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+// let c = +prompt("3son")
+
+// let min = 0;
+
+// if (a > b) {
+//     min = b;
+// }else{
+//     min = a;
+// }
+
+// if (b > c) {
+//     min = c
+// }
+
+// console.log(min);
+
+// BOOLEAN 1-misol
+// let num = +prompt(sonni kiriting)
+
+// if(num>0){
+//     console.log(musbat son);
+// }else{
+//     console.log(manfiy son);
+
+// }
+
+// BOOLEAN  2-misol
+// let num =+prompt(sonni kiriting)
+
+// if (num % 2 == 1) {
+//     console.log(toq son);
+
+// }else{
+//     console.log(toq son emas);
+
+// }
+
+// BOOLEAN  3-misol
+
+// let num =+prompt(sonni kiriting)
+
+// if (num % 2 == 0) {
+//     console.log(juft son);
+
+// }else{
+//     console.log(juft son emas);
+
+// }
+
+// BOOLEAN  4-misol
+
+// let a =+prompt(1-sonni kiriting)
+// let b =+prompt(2-sonni kiriting)
+
+// if(a>2 && b<=3){
+//     console.log(jumla togri);
+
+// }else{
+//     console.log(jumla notogri);
+// }
+
+// BOOLEAN  5-misol
+// let a =+prompt(1-sonni kiriting)
+// let b =+prompt(2-sonni kiriting)
+
+// if(a >= 0  b < -2){
+//     console.log(`jumla togri `);
+
+// }else{
+//     console.log(`jumla notogri `);
+// }
+
+// BOOLEAN  6-misol
+//  let a =+prompt(`1 - sonni kiriting `)
+// let b =+prompt(`2 - sonni kiriting `)
+// let c =+prompt(`3 - sonni kiriting `)
+
+// if(a<=b && b<=c){
+//     console.log(`jumla togri `);
+// }else{
+//     console.log(`jumla notogri `);
+// }
+
+// BOOLEAN  7-misol
+
+// let a =+prompt(`1 - sonni kiriting `)
+//  let b =+prompt(`2 - sonni kiriting `)
+//  let c =+prompt(`3 - sonni kiriting `)
+
+//  if ((b > a && b < c)  (b > c && b < a)) {
+//     console.log("b soni a va c orasida yotadi");
+// } else {
+//     console.log("b soni a va c orasida yotmaydi");
+// }
+
+// BOOLEAN  8-misol
+// let a =+prompt(1-sonni kiriting)
+//  let b =+prompt(2-sonni kiriting)
+
+//  if(a % 2 == 1 && b % 2 == 1 ){
+// console.log(toq sonlar );
+
+//  }else{
+//     console.log(toq sonlar emas);
+//  }
+
+// BOOLEAN  9-misol
+
+// let a =+prompt(1-sonni kiriting)
+//   let b =+prompt(2-sonni kiriting)
+
+//   if(a % 2 == 1  b % 2 == 1 ){
+//  console.log(`toq son `);
+
+//  }else{
+//     console.log(`toq son emas `);
+// }
+
+// BOOLEAN  10-misol
+
+// let a =+prompt(`1 - sonni kiriting `)
+// let b =+prompt(`2 - sonni kiriting `)
+
+// if(a % 2 == 1 && b % 2 == 0  a % 2 == 0 && b % 2 == 1 ){
+//  console.log(jumla togri );
+
+// }else{
+//     console.log(jumla notogri);
+//  }
